f618c945545d43398c54f3f6dfac65e2df2a03b5
Adam Helf <adam-helf@uiowa.edu>
2019-10-08 11:33:17 -0500

    added html files, static, and template folders

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "hello World"
+    return render_template('index.html', pageTitle='Loan Calculator')
 
 @app.route('/adam')
 def adam():
